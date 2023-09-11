ffe0ba99c71f8224cbb8edbef8e8a884ef9d665d
MrCurtis <4184070+MrCurtis@users.noreply.github.com>
2023-09-11 12:53:19 +0000

    Add second table to setup function

--- data_setup.py
+++ data_setup.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, create_engine, Integer, MetaData, Table
+from sqlalchemy import Column, create_engine, ForeignKey, Integer, MetaData, Table
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 
 def setup_data():
@@ -7,5 +7,11 @@ def setup_data():
         "table_a",
         metadata_object,
         Column("id", Integer, primary_key=True),
+    )
+    table_b = Table(
+        "table_b",
+        metadata_object,
+        Column("id", Integer, primary_key=True),
+        Column("a_id", ForeignKey("table_a.id"), nullable=False),
     )
     metadata_object.create_all(engine)
