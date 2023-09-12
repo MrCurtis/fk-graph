382654f4d50fc3780b7c1833c8057bed75cf6e29
MrCurtis <4184070+MrCurtis@users.noreply.github.com>
2023-09-12 12:15:16 +0000

    Setup data directly for tests.
    We want to test a variety of different scenarios, so no longer want to
    use the `setup_data` function.

--- test_graph.py
+++ test_graph.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from networkx import Graph, is_isomorphic
-from sqlalchemy import create_engine, text
+from sqlalchemy import Column, create_engine, ForeignKey, insert, Integer, MetaData, Table
 
 from data_setup import setup_data
 
@@ -15,6 +15,7 @@ class GetGraphTests(TestCase):
         setup_data(self.engine)
 
     def test_can_build_from_reverse_foreign_key_relations(self):
+        self._create_db_with_foreign_key_relations(self.engine)
         node_1 = Node(table="table_a", primary_key=1)
         node_2 = Node(table="table_b", primary_key=1)
         node_3 = Node(table="table_b", primary_key=2)
@@ -29,3 +30,33 @@ class GetGraphTests(TestCase):
 
         with self.subTest():
             self.assertTrue(is_isomorphic(graph, expected_graph))
+
+    def _create_db_with_foreign_key_relations(self, engine):
+        metadata_object = MetaData()
+        table_a = Table(
+            "table_a",
+            metadata_object,
+            Column("id", Integer, primary_key=True),
+        )
+        table_b = Table(
+            "table_b",
+            metadata_object,
+            Column("id", Integer, primary_key=True),
+            Column("a_id", ForeignKey("table_a.id"), nullable=False),
+        )
+        metadata_object.create_all(engine)
+        with engine.connect() as conn:
+            conn.execute(
+                insert(table_a),
+                [
+                    {"id": 1},
+                ]
+            )
+            conn.execute(
+                insert(table_b),
+                [
+                    {"id": 1, "a_id": 1},
+                    {"id": 2, "a_id": 1},
+                ]
+            )
+            conn.commit()
