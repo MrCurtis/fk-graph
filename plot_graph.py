f789b368f661ab4f7859515e0c467399bb38507f
JT <jcthomas000@gmail.com>
2023-09-12 16:04:45 +0100

    plot graph version 2, lots of internal changes

--- plot_graph.py
+++ plot_graph.py
@@ -1,7 +1,7 @@
-from plotly_functions import process_graph, plot_v1
+from plotly_functions import plot_v2
 import networkx as nx
 import plotly as ply
 
 def plot(graph:nx.Graph) -> ply.graph_objs.Figure:
-    fig = plot_v1(*process_graph(graph))
+    fig = plot_v2(graph)
     return fig
